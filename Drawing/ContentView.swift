2f2f44e91d3e8e8488da7d8998f5a9eeeabf640b
Milo Wyner <milowyner@gmail.com>
2021-08-12 21:02:37 -0700

    Create flower shape with sliders to control the petal offset and width to create a cool effect

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var petalOffset: CGFloat = -20.0
+    @State private var petalWidth: CGFloat = 100.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .fill(Color.red, style: FillStyle(eoFill: true))
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...400)
+                .padding([.horizontal, .bottom])
+            
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+        }
     }
 }
 
