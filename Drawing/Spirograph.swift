336b073854e9b79d479160959f079b5a17db5420
Milo Wyner <milowyner@gmail.com>
2021-08-13 16:13:12 -0700

    Create spirograph shape (just copied cause it's a lot of algorithmic code)

---
+++ Drawing/Spirograph.swift
@@ -0,0 +1,61 @@
+//
+//  Spirograph.swift
+//  Drawing
+//
+//  Created by Milo Wyner on 8/13/21.
+//
+
+import SwiftUI
+
+struct Spirograph: Shape {
+    let innerRadius: Int
+    let outerRadius: Int
+    let distance: Int
+    let amount: CGFloat
+    
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+
+        while b != 0 {
+            let temp = b
+            b = a % b
+            a = temp
+        }
+
+        return a
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        let divisor = gcd(innerRadius, outerRadius)
+        let outerRadius = CGFloat(outerRadius)
+        let innerRadius = CGFloat(innerRadius)
+        let distance = CGFloat(distance)
+        let difference = innerRadius - outerRadius
+        let endPoint = ceil(2 * CGFloat.pi * outerRadius / CGFloat(divisor)) * amount
+
+        var path = Path()
+
+        for theta in stride(from: 0, through: endPoint, by: 0.01) {
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+
+            x += rect.width / 2
+            y += rect.height / 2
+
+            if theta == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            } else {
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
+        }
+
+        return path
+    }
+}
+
+//struct Spirograph_Previews: PreviewProvider {
+//    static var previews: some View {
+//        Spirograph()
+//    }
+//}
