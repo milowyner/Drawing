da93ad37bc3382cbbd214509c15a1dcd06286c61
Milo Wyner <milowyner@gmail.com>
2021-08-13 18:24:02 -0700

    Complete challenge #1: create an arrow shape out of a triangle and rectangle

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,51 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var innerRadius = 125.0
-    @State private var outerRadius = 75.0
-    @State private var distance = 25.0
-    @State private var amount: CGFloat = 1.0
-    @State private var hue = 0.6
-
     var body: some View {
-        VStack(spacing: 0) {
-            Spacer()
-
-            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
-                .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
-                .frame(width: 400, height: 400)
-
-            Spacer()
-
-            Group {
-                Text("Inner radius: \(Int(innerRadius))")
-                Slider(value: $innerRadius, in: 10...150, step: 1)
-                    .padding([.horizontal, .bottom])
-
-                Text("Outer radius: \(Int(outerRadius))")
-                Slider(value: $outerRadius, in: 10...150, step: 1)
-                    .padding([.horizontal, .bottom])
-
-                Text("Distance: \(Int(distance))")
-                Slider(value: $distance, in: 1...150, step: 1)
-                    .padding([.horizontal, .bottom])
-
-                Text("Amount: \(amount, specifier: "%.2f")")
-                Slider(value: $amount)
-                    .padding([.horizontal, .bottom])
-
-                Text("Color")
-                Slider(value: $hue)
-                    .padding(.horizontal)
-            }
-            
-            Button("Animate") {
-                amount = 0.0
-                withAnimation(.linear(duration: 5)) {
-                    amount = 1.0
-                }
-            }
-        }
+        Arrow()
+            .frame(width: 200, height: 200)
     }
 }
 
